bcce0264936877d1c2597c302b25f3fdd2ac3707
owolfhu1 <owolfhu1@gmail.com>
2017-09-25 12:48:11 -0400

    Create FoodType.java

---
+++ FoodType.java
@@ -0,0 +1,11 @@
+
+/*
+ * fileName : 
+ * author : Orion Wolf-Hubbard
+ * date : 9/25/2017
+ * purpose : To define the type of Food.java
+ */
+
+public enum FoodType {
+    PIZZA, SOUP, POPCORN, HOT_POCKET, MUFFIN,
+}
