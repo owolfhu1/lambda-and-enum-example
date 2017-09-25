51868837114355520cc8a635ec5d4f771466baf9
owolfhu1 <owolfhu1@gmail.com>
2017-09-25 12:48:39 -0400

    Create FoodState.java

---
+++ FoodState.java
@@ -0,0 +1,11 @@
+
+/*
+ * fileName : FoodState.java
+ * author : Orion Wolf-Hubbard
+ * date : 9/25/2017
+ * purpose : to define how cooked a Food.java is
+ */
+
+public enum FoodState {
+    FROZEN, THAWED, COOKED, BURNED, NUKED
+}
